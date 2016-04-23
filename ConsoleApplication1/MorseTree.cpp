367bc3413b5ca802a402479b1cb11c1b90197565
JacobBothell <jacob20140273@gmail.com>
2016-04-23 17:45:55 -0500

    Everything but encode is done NO TESTING

--- ConsoleApplication1/MorseTree.cpp
+++ ConsoleApplication1/MorseTree.cpp
@@ -1,2 +1,170 @@
 #include "MorseTree.h"
 
+MorseTree::MorseTree()
+{
+	//makes tree of apropriate size
+	morse.initTreeHeight(4);
+}
+MorseTree::MorseTree(fstream morse_file)
+{
+	//if file is not yet open it returns with no tree created
+	if (!morse_file.is_open())
+	{
+		return;
+	}
+
+	//makes tree of apropriate size
+	morse.initTreeHeight(4);
+	string line;
+	char character;
+	string code;
+
+	//reads in next character
+	while (morse_file >> line)
+	{
+		//splits the input apart into sections
+		character = line[1];
+		line.erase(1);
+		code = line;
+
+		//inserts the line into the tree
+		insert(character, code);
+	}
+
+	//closes file when done
+	morse_file.close();
+
+	return;
+}
+
+//for decoding a string of '.' and '-'
+//Bothe return zero "0" if they fail
+char MorseTree::decode_char(string code_char)
+{
+	//weeds out characters that can't exist in morse code
+	if (code_char == "._._" || code_char == "___." || code_char == "____" || code_char == "__..")
+		return '0';
+
+	string::iterator sit;
+	morse.setCursorToRoot();
+
+	for (sit = code_char.begin(); sit != code_char.begin(); sit++)
+	{
+		if (*sit == '.')
+		{
+			if (morse.goLeft())
+				continue;
+			//if the position exists then it goes back to the top
+			else
+				return '0';
+			//position dosen't exist and you are stupid
+		}
+		else if (*sit == '_')
+		{
+			if (morse.goRight())
+				continue;
+			//if the position exists then it goes back to top
+			else
+				return '0';
+			//position dosen't exist and you are stupid
+		}
+		else
+			return '0';
+	}
+
+	return morse.getCursorData();
+}
+string	MorseTree::decode_str(string code_str)
+{
+	if (code_str == "")
+		return "0";
+
+	string character = "";
+	string decoded_str = "";
+	string::iterator sit;
+
+	for (sit = code_str.begin(); sit != code_str.end(); sit++)
+	{
+		if (*sit == ' ')
+		{
+			decoded_str += decode_char(character);
+			character.clear();
+		}
+		else if (*sit == '.' || *sit == '_')
+		{
+			character += *sit;
+		}
+		else
+			return "0";
+	}
+	decoded_str += decode_char(character);
+
+	return decoded_str;
+}
+
+//for encoding a string to morse
+string MorseTree::encode_char(char character)
+{
+
+}
+string MorseTree::encode_str(string str)
+{
+	
+}
+
+//inserts a character based on a string of '.' and '-'
+bool MorseTree::insert(char character, string code)
+{
+	if (code == "") //checks for bad input
+	{
+		return false;
+	}
+
+	//weeds out characters that can't exist in morse code
+	if (code == "._._" || code == "___." || code == "____" || code == "__..")
+		return false;
+
+	morse.setCursorToRoot(); //loads root of tree
+	string::iterator sit;	//starts iterator for code string
+
+	//ther are 2 possible characters
+	//this loop searches for the location of the insert using joshes built in cursor
+	for (sit = code.begin(); sit != code.begin(); sit++)
+	{
+		if (*sit == '.')
+		{
+			if (morse.goLeft())
+				continue;
+			//if the position exists then it goes back to the top
+			else
+				return false;
+			//position dosen't exist and you are stupid
+		}
+		else if (*sit == '_')
+		{
+			if (morse.goRight())
+				continue;
+			//if the position exists then it goes back to top
+			else
+				return false;
+			//position dosen't exist and you are stupid
+		}
+		else
+			return false;
+	}
+
+	//actual insert of the item
+	//pulls item from location, adds character, and re-sets it
+	CBTNode* current = morse.getCursor();
+	//checks if the item has already been inserted
+	if (current->data == 0)
+	{
+		current->data = character;
+		morse.setCursor(current);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
--- ConsoleApplication1/MorseTree.h
+++ ConsoleApplication1/MorseTree.h
@@ -2,12 +2,35 @@
 #define MORSETREE_H
 
 #include "BinTree.h"
+#include <string>
+#include <fstream>
+using namespace std;
 
-class MorseTree {
+//this class is for making a tree for morse coded characters
+//where going left is encoded as a period '.' and going right is encoded as an underscore '_'
+
+class MorseTree{
 private:
-	//CharBinTree Decoder;
+	CharBinTree morse;
 public:
+	//makes an empty tree of the correct size for morse code
 	MorseTree();
+	//can read a file and build from there
+	//closes the file when done
+	MorseTree(fstream morse_file);
+
+	//for decoding a string of periods '.' and underscores '_'
+	//both return zero "0" if they fail
+	char decode_char(string code_char);
+	//assumes string is NOT terminated by a space
+	string	decode_str(string code_str);
+
+	//for encoding a string to morse
+	string encode_char(char character);
+	string encode_str(string str);
+
+	//inserts a character based on a string of periods '.' and underscores '_'
+	bool insert(char character, string code);
 };
 
 #endif
